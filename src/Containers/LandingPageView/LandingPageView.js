b528309121d0e04dab499bc479c6cc6d756d9cfd
Jeandre Melaria <J.melaria@hotmail.com>
2018-04-13 16:28:09 +0200

    <Add>: ReactMaterialize css

---
+++ src/Containers/LandingPageView/LandingPageView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button, Icon} from 'react-materialize';
+
+const landingPageView = () => {
+  return (
+    <div>
+      <h1>Landing page view </h1>
+      <p>BG</p>
+      <p>Buttons</p>
+      <p>Greetings</p>
+      <Button waves='light'>button</Button>
+    </div>
+      );
+};
+
+export default landingPageView; 
