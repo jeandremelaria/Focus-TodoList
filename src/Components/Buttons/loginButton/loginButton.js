559fd13354442260e246e0d7a059a95789024139
Jeandre Melaria <j.melaria@hotmail.com>
2018-04-25 20:03:21 +0200

    Refractor: LoginButton.js, Added state to buttons and return them using the map function

--- src/Components/Buttons/loginButton/loginButton.js
+++ src/Components/Buttons/loginButton/loginButton.js
@@ -5,18 +5,18 @@ import './LoginButton.css';
 class LoginButton extends Component {
   state = {
     buttons:[
-      {name:'Google', class:'buttonGoogle--color button button__margin--top'},
-      {name:'Facebook', class:'buttonFacebook--color button'},
-      {name:'Email', class:'buttonEmail--color button button__margin--bottom'}
+      {id:'google', name:'Google', class:'buttonGoogle--color button button__margin--top'},
+      {id:'facebook', name:'Facebook', class:'buttonFacebook--color button'},
+      {id:'email', name:'Email', class:'buttonEmail--color button button__margin--bottom'},
     ]
   }
   render() {
     const button = this.state.buttons;
     return (
     <div>
-      <Button waves='light' className={button[0].class}>{button[0].name}</Button>
-      <Button waves='light' className={button[1].class}>{button[1].name}</Button>
-      <Button waves='light' className={button[2].class}>{button[2].name}</Button>
+      {this.state.buttons.map(button => 
+        <Button waves='light' className={button.class}>{button.name}</Button>
+      )}
     </div>
   );
   }
